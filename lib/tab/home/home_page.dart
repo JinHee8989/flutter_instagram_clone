e753332571fc700430dcec8c895fcaeb700258aa
jinhee <wlsgml8989@gmail.com>
2023-03-29 22:39:33 +0900

    Update home_page.dart : - UI 작성

--- lib/tab/home/home_page.dart
+++ lib/tab/home/home_page.dart
@@ -7,8 +7,71 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Instagram Clone')),
-      body: Column(
-        children: const [Text('Instagram에 오신 것을 환영합니다.')],
+      body: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            SizedBox(height: 20),
+            Text(
+              'Instagram에 오신 것을 환영합니다.',
+              style: TextStyle(fontSize: 24),
+            ),
+            SizedBox(height: 20),
+            Text('사진과 동영상을 보려면 팔로우하세요'),
+            SizedBox(height: 20),
+            Card(
+              elevation: 4.0,
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(children: [
+                  SizedBox(height: 8),
+                  SizedBox(
+                    width: 80,
+                    height: 80,
+                    child: CircleAvatar(
+                      backgroundImage: NetworkImage(
+                          'https://cdn.sisamagazine.co.kr/news/photo/202112/420043_422171_2028.jpg'),
+                    ),
+                  ),
+                  SizedBox(height: 8),
+                  Text(
+                    'flutter@test.com',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  Text('닉네임'),
+                  SizedBox(height: 8),
+                  Row(
+                    mainAxisSize: MainAxisSize.min,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Image.network(
+                          'https://www.jeonmae.co.kr/news/photo/202206/895433_587294_2748.png',
+                          width: 70,
+                          height: 70,
+                          fit: BoxFit.cover),
+                      SizedBox(width: 4),
+                      Image.network(
+                          'https://thumb.mt.co.kr/06/2021/02/2021021913564958785_1.jpg/dims/optimize/',
+                          width: 70,
+                          height: 70,
+                          fit: BoxFit.cover),
+                      SizedBox(width: 4),
+                      Image.network(
+                          'https://image.edaily.co.kr/images/Photo/files/NP/S/2022/02/PS22022100167.jpg',
+                          width: 70,
+                          height: 70,
+                          fit: BoxFit.cover),
+                    ],
+                  ),
+                  SizedBox(height: 8),
+                  Text('Facebook 친구'),
+                  SizedBox(height: 8),
+                  ElevatedButton(onPressed: () {}, child: Text('팔로우'))
+                ]),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
