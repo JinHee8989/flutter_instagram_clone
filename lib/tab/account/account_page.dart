a808d757a5137369494ec5452773b2afc40887fc
jinhee <wlsgml8989@gmail.com>
2023-04-05 22:42:26 +0900

    firebase 연동

--- lib/tab/account/account_page.dart
+++ lib/tab/account/account_page.dart
@@ -5,6 +5,94 @@ class AccountPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Instagram Clone'),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.exit_to_app),
+          ),
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(
+              children: [
+                Stack(
+                  children: [
+                    const SizedBox(
+                      width: 80,
+                      height: 80,
+                      child: CircleAvatar(
+                        backgroundImage: NetworkImage(
+                            'https://cdn.sisamagazine.co.kr/news/photo/202112/420043_422171_2028.jpg'),
+                      ),
+                    ),
+                    Container(
+                      width: 80,
+                      height: 80,
+                      alignment: Alignment.bottomRight,
+                      child: Stack(
+                        alignment: Alignment.center,
+                        children: const [
+                          SizedBox(
+                            width: 28,
+                            height: 28,
+                            child: FloatingActionButton(
+                              onPressed: null,
+                              backgroundColor: Colors.white,
+                            ),
+                          ),
+                          SizedBox(
+                            width: 25,
+                            height: 25,
+                            child: FloatingActionButton(
+                              onPressed: null,
+                              child: Icon(Icons.add),
+                            ),
+                          ),
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+                const SizedBox(
+                  height: 8,
+                ),
+                const Text(
+                  '변진희',
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                  ),
+                )
+              ],
+            ),
+            Column(
+              children: [
+                Text('3', style: TextStyle(fontSize: 18)),
+                Text('게시물', style: TextStyle(fontSize: 18)),
+              ],
+            ),
+            Column(
+              children: [
+                Text('0', style: TextStyle(fontSize: 18)),
+                Text('팔로워', style: TextStyle(fontSize: 18)),
+              ],
+            ),
+            Column(
+              children: [
+                Text('0', style: TextStyle(fontSize: 18)),
+                Text('팔로잉', style: TextStyle(fontSize: 18)),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/tab/search/search_page.dart
+++ lib/tab/search/search_page.dart
@@ -1,10 +1,47 @@
 import 'package:flutter/material.dart';
 
+import '../../create/create_page.dart';
+
 class SearchPage extends StatelessWidget {
   const SearchPage({Key? key}) : super(key: key);
 
+  final List<String> _urls = const [
+    'https://image.edaily.co.kr/images/Photo/files/NP/S/2022/02/PS22022100167.jpg',
+    'https://thumb.mt.co.kr/06/2021/02/2021021913564958785_1.jpg/dims/optimize/',
+    'https://www.jeonmae.co.kr/news/photo/202206/895433_587294_2748.png',
+    'https://img.wowtv.co.kr/wowtv_news/dnrs/20220915/2022091514434502712d3244b4fed182172185139.jpg'
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => const CreatePage()),
+          );
+        },
+        child: const Icon(Icons.create),
+      ),
+      appBar: AppBar(title: Text('Instagram Clone')),
+      body: Padding(
+        padding: const EdgeInsets.all(2.0),
+        child: GridView.builder(
+            itemCount: _urls.length,
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 3,
+              mainAxisSpacing: 2.0,
+              crossAxisSpacing: 2.0,
+            ),
+            itemBuilder: (BuildContext context, int index) {
+              final url = _urls[index];
+              return Image.network(
+                url,
+                fit: BoxFit.cover,
+              );
+            }),
+      ),
+    );
   }
 }
